2badc1cb11d55f46a7a5f0e9a362bd3f041b0b17
Tai An Su <a@tai.su>
2026-07-29 14:50:02 +0800

    Use regular fn keys in mac, let karabiner to handle media functions

--- keyboards/keychron/q11/ansi_encoder/keymaps/tai/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/tai/keymap.c
@@ -14,7 +14,7 @@ enum custom_keycodes {
     HYPER_MOD = SAFE_RANGE,
     MEH_MOD,
     CAG_MOD,
-    PTT_MOD,
+    LCA_MOD,
 };
 
 #define KC_TASK LGUI(KC_TAB)
@@ -37,7 +37,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return process_modifier_key(record, MOD_MEH);
         case CAG_MOD:
             return process_modifier_key(record, MOD_LCTL | MOD_LALT | MOD_LGUI);
-        case PTT_MOD:
+        case LCA_MOD:
             return process_modifier_key(record, MOD_LCTL | MOD_LALT);
     }
 
@@ -46,12 +46,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_91_ansi(
-        KC_MUTE,       KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RM_VALD,   RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   KC_MUTE,
+        KC_MUTE,       KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,  KC_DEL,    KC_MUTE,
         MEH(KC_SPC),   KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         HYPER_MOD,     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         MEH_MOD,       KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         CAG_MOD,       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        PTT_MOD,       KC_GLOBE, KC_LCTL,  KC_LOPT,  KC_LCMD,            KC_SPC,                        KC_SPC,             KC_RCMD,  KC_RCTL, MO(MAC_FN),  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        LCA_MOD,       KC_F20, KC_LCTL,  KC_LOPT,  KC_LCMD,            KC_SPC,                        KC_SPC,             KC_RCMD,  KC_RCTL, MO(MAC_FN),  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_91_ansi(
         RM_TOGG,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,  _______,  RM_TOGG,
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         HYPER_MOD,     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         MEH_MOD,       KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         CAG_MOD,       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        PTT_MOD,       MO(WIN_FN), KC_LCTL, KC_LALT, KC_LWIN,            KC_SPC,                        KC_SPC,             KC_RWIN,  KC_RCTL, MO(WIN_FN),  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        LCA_MOD,       MO(WIN_FN), KC_LCTL, KC_LALT, KC_LWIN,            KC_SPC,                        KC_SPC,             KC_RWIN,  KC_RCTL, MO(WIN_FN),  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_91_ansi(
         RM_TOGG,  _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RM_VALD,   RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  RM_TOGG,
